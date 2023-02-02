fea0c6740add277378d2ec8ec10e8b825a6fc8f0
Abolfazl <abolfazlm181920@gmail.com>
2023-02-02 13:16:28 +0330

    [ui]: add form ui

--- src/features/TodoList.jsx
+++ src/features/TodoList.jsx
@@ -40,7 +40,23 @@ const TodoList = () => {
         setNewTodo('')
     }
 
-    
+    const newItemSection = (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="new-todo">Enter a new todo item</label>
+            <div className="new-todo">
+                <input
+                    type="text"
+                    id="new-todo"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    placeholder="Enter new todo"
+                />
+            </div>
+            <button className="submit">
+                <FontAwesomeIcon icon={faUpload} />
+            </button>
+        </form>
+    )
 
     return (
         <div>
